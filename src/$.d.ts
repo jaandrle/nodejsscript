3e6a6721efb4171e8924ff4fc9a469cab1e7269e
Jan Andrle <andrle.jan@centrum.cz>
2022-11-25 15:09:15 +0100

    :pencil:

--- src/$.d.ts
+++ src/$.d.ts
@@ -11,7 +11,14 @@ export namespace Dollar{
 	/**
 	 * This is small helper function to determine if current script file was launched as main one.
 	 * ```js
+	 * //nodejsscript main.js
+	 * 
+	 * //main.js
 	 * if($.isMain(import.meta)) echo("This is main script");
+	 * import 'nomain.js';
+	 * 
+	 * //nomain.js
+	 * if($.isMain(import.meta)) echo("This is NOT main script ⇒ never echo");
 	 * ```
 	 * @category Public
 	 * */
