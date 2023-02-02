b7c7b8c05293fe591c8b1af860a527166b1d4f6c
Jan Andrle <andrle.jan@centrum.cz>
2023-02-02 16:35:39 +0100

    :bug: again `;` (wrong order when returning quotes)

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -110,15 +110,14 @@ function jsconfigTypes(){
 function runEval(is_print){
 	let input= argv.splice(2, 1)[0] ?? "";
 	if(is_print){
-		const m= randomUUID();
+		const m= "--§--"+randomUUID().replaceAll("-", "")+"--§--";
 		const store_quotes= [];
 		input= input.replace(/("([^"]|(?<=\\)")+"|'([^']|(?<=\\)')+'|`([^`]|(?<=\\)`)+`)/g,
 			//temporary remove quotes (can contain ';')
 			f=> (store_quotes.push(f), m));
-		const m_regexp= new RegExp(m.toString().replace(".", "\\."), "g");
+		const m_regexp= new RegExp(m, "g");
 		let out_arr= input.split(";").reverse()
-			//return quotes (in reverse order)
-			.map(v=> v.replace(m_regexp, ()=> store_quotes.shift()));
+			.map(/* return quotes */ v=> v.replace(m_regexp, ()=> store_quotes.pop()));
 		if(out_arr[0].trim()==="") out_arr.shift();
 		let pre= "";
 		let out= out_arr[0]?.trim();
